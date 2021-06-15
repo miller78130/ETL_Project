2f8e8913b27a8e7d8f1dad61290be7e52f2c6f9b
Veronica O <veronica.o.garcia@gmail.com>
2021-06-15 10:11:37 -0500

    relocated schemata and added ETLJupyter.pynb

---
+++ Alcohol_and_Happiness/Resources/.ipynb_checkpoints/DB Schemata-checkpoint.sql
@@ -0,0 +1,47 @@
+CREATE TABLE worldhappinessreport2021(
+	country VARCHAR,
+	reginal_indicator VARCHAR,
+	ladder_score Numeric,
+	standard_error_of_ladder Numeric,
+	upperwhisker Numeric,
+	lowerwhisker Numeric,
+    logged_gdp_per_capita Numeric,
+    social_suppor Numeric,
+    healthy_life_expectancy Numeric,
+    freedom_to_make_life_choices Numeric,
+    generosity Numeric,
+    perceptions_of_corruption Numeric,
+    ladder_score_in_dystopia Numeric,
+    dystopia_plus_residual Numeric
+);
+
+
+
+CREATE TABLE HappinessAlcoholConsumption(
+	country VARCHAR,
+	region VARCHAR,
+	hemisphere Numeric,
+	happinessscore Numeric,
+	hdi Numeric,
+	gdp_percapita Numeric,
+	beer_percapita Numeric,
+	spirit_percapita Numeric,
+	wine_percapita Numeric,
+
+);
+
+CREATE TABLE LifeExpectancy(
+	countryname VARCHAR,
+	IndicatorName VARCHAR,
+	Year_2019 Numeric
+	
+);
+
+
+SELECT LE.countryname, HP.HappinessScore, WH.Healthy_life_expectancy, LE.year_2019, WH.social_support, WH.Generosity, HP.beer_percapita, HP.spirit_percapita, HP.wine_percapita
+FROM worldhappinessreport2021 as WH
+right JOIN HappinessAlcoholConsumption as HP    
+ON WH.country_name=HP.country
+left join lifeexpectancy as LE
+ON WH.country_name=LE.countryname
+ORDER BY "happinessscore" DESC
